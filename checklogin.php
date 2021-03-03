eb7aa213f741d45c238ede69b1dd134f28985827
Nikolas Doğan Halil <nikolasdogan@gmail.com>
2021-03-03 14:17:38 +0300

    Create checklogin.php

---
+++ checklogin.php
@@ -0,0 +1,15 @@
+<?php
+function check_login()
+{
+if(strlen($_SESSION['login'])==0)
+	{	
+		$host=$_SERVER['HTTP_HOST'];
+		$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+		$extra="index.php";		
+		$_SESSION["login"]="";
+		header("Location: http://$host$uri/$extra");
+	}
+}
+?>
+
+// index.php de ve dashboard.php de her sayfa yenilendiğinde kullanıcının login sessionları kontrol ediliyor.
